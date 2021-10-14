f84d379a4f3ff91a30b3843473a52d9239d3e294
Tobias Schweiger <tschwei@siue.edu>
2021-10-13 21:13:36 -0500

    Initial Commit

---
+++ Alienated/AlienatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlienatedApp.swift
+//  Alienated
+//
+//  Created by Tobi Schweiger on 10/13/21.
+//
+
+import SwiftUI
+
+@main
+struct AlienatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
